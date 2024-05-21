3e7c7719e04b74ef17aa2081a3a962a0bbe903c9
rnMik4 <rnrafaelsilva@gmail.com>
2024-05-20 22:45:30 -0300

    CRUD Usuarios
    Crud completo de usuarios + login + Generic Mapper para DTOS

--- tinder_books/src/main/java/br/com/rnrafa/services/UsuariosService.java
+++ tinder_books/src/main/java/br/com/rnrafa/services/UsuariosService.java
@@ -1,50 +1,59 @@
 package br.com.rnrafa.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.rnrafa.dto.UsuariosRequestDTO;
+import br.com.rnrafa.dto.UsuariosResponseDTO;
 import br.com.rnrafa.entity.Usuarios;
 import br.com.rnrafa.exceptions.ResourceNotFoundException;
 import br.com.rnrafa.repositories.UsuarioRepository;
 
 @Service
 public class UsuariosService {
+	
+	@Autowired
+	private GenericMapper mapper;
+	
 	@Autowired(required = false)
 	UsuarioRepository repository;
 	
-	public List<Usuarios> findAll(){
-		return repository.findAll();
+	public List<UsuariosResponseDTO> findAll(){
+		return mapper.entidadeParaDTO(repository.findAll(), UsuariosResponseDTO.class);
 	}
 	
-	public Usuarios findById(Long id) {
-		return repository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("User not found!"));
+	public UsuariosResponseDTO findById(Long id) {
+		Usuarios entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Usuario não encontrado"));
+		return mapper.entidadeParaDTO(entity, UsuariosResponseDTO.class);
 	}
 	
-	public Usuarios create(Usuarios usuario) {
-		return repository.save(usuario);
-	}
-	
-	public Usuarios update(Usuarios usuario) {
-		Usuarios entity = repository.findById(usuario.getId())
-				.orElseThrow(() -> new ResourceNotFoundException("User not found!"));
+	public UsuariosResponseDTO create(UsuariosRequestDTO usuarioDTO) {
+		//Verifica se o e-mail ja existe na base
+		Usuarios entity = repository.findByEmail(usuarioDTO.getEmail());
+		if(entity != null) {
+			throw new ResourceNotFoundException("Email já cadastrado");
+		}
 		
-		entity.setNomeCompleto(usuario.getNomeCompleto());
-		entity.setEmail(usuario.getEmail());
-		entity.setCep(usuario.getCep());
-		entity.setRua(usuario.getRua());
-		entity.setCidade(usuario.getCidade());
-		entity.setUf(usuario.getUf());
-		entity.setLatitude(usuario.getLatitude());
-		entity.setLongitude(usuario.getLongitude());
-		entity.setDdd(usuario.getDdd());
-		entity.setTelefone(usuario.getTelefone());
-		entity.setAtivo(usuario.getAtivo());
-		entity.setAceiteTermos(usuario.getAceiteTermos());
+		Usuarios usuarioSalvar = new Usuarios();
+		BeanUtils.copyProperties(usuarioDTO, usuarioSalvar);
+		usuarioSalvar.setDatacriacao(LocalDateTime.now());
+		return mapper.entidadeParaDTO(repository.save(usuarioSalvar), UsuariosResponseDTO.class);
+	}
+	
+	public UsuariosResponseDTO update(UsuariosRequestDTO usuarioDTO, Long id) {
+		Usuarios entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Usuario não encontrado"));
 		
-		return repository.save(entity);
+		BeanUtils.copyProperties(usuarioDTO, entity);
+		
+		entity.setId(id);
+		
+		return mapper.entidadeParaDTO(repository.save(entity), UsuariosResponseDTO.class);
 	}
 	
 	public void delete(Long id) {
@@ -52,4 +61,15 @@ public class UsuariosService {
 				.orElseThrow(() -> new ResourceNotFoundException("User not found!"));
 		repository.delete(entity);
 	}
+	
+	public UsuariosResponseDTO login(String email, String senha) {
+		Usuarios entity = repository.findByEmailAndSenha(email, senha);
+		if(entity == null) {
+			throw new ResourceNotFoundException("Usuário ou Senha incorretos"); 
+		}
+		
+		return mapper.entidadeParaDTO(entity, UsuariosResponseDTO.class);
+	}
+	
+	
 }
