2f8daaf7b99097d2a96cbabbd41c57730f701783
rnMik4 <rnrafaelsilva@gmail.com>
2024-05-23 20:43:25 -0300

    merge jean rafa

---
+++ tinder_books/src/main/java/br/com/rnrafa/dto/UsuariosRequestDTO.java
@@ -0,0 +1,110 @@
+package br.com.rnrafa.dto;
+
+
+public class UsuariosRequestDTO {
+	private String email;
+	private String nomeCompleto;
+	private String cep;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	private Float latitude;
+	private Float longitude;
+	private Integer ddd;
+	private String telefone;
+	private String ativo;
+	private String aceiteTermos;
+	private String senha;
+	
+	
+	
+	
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	public Float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Float latitude) {
+		this.latitude = latitude;
+	}
+	public Float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Float longitude) {
+		this.longitude = longitude;
+	}
+	public Integer getDdd() {
+		return ddd;
+	}
+	public void setDdd(Integer ddd) {
+		this.ddd = ddd;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getAtivo() {
+		return ativo;
+	}
+	public void setAtivo(String ativo) {
+		this.ativo = ativo;
+	}
+	public String getAceiteTermos() {
+		return aceiteTermos;
+	}
+	public void setAceiteTermos(String aceiteTermos) {
+		this.aceiteTermos = aceiteTermos;
+	}
+	
+	
+
+}
